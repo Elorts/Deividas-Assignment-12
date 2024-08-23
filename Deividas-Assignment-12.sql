d1148e2f43d90fa6c4ea3e36a22928af4c22bb6e
deividas strole <deiwis@yahoo.com>
2024-08-22 20:57:30 -0700

    Changed tables structure and updated code

--- Deividas-Assignment-12.sql
+++ Deividas-Assignment-12.sql
@@ -1,44 +1,83 @@
+create database pizza_order_DB2;
 
--- **************************************** \/ Table creation and data population code below \/ *******************************************
+USE pizza_order_DB2;
+
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+	customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS orders;
 
 CREATE TABLE orders (
-	customer VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(50),
-    order_date_time VARCHAR(50), 						
-    dish_name VARCHAR(50) NOT NULL
+	customer_id INT NOT NULL,     
+    dish_id INT NOT NULL,
+    order_date_time DATETIME, 						
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
+
+DROP TABLE IF EXISTS pizzas;
 
 CREATE TABLE pizzas (
+	dish_id INT PRIMARY KEY,
 	dish_name VARCHAR(100) NOT NULL,
-    price DECIMAL(4, 2)
+    dish_price DECIMAL(4, 2)
 );
 
-INSERT INTO orders (customer, phone_number, order_date_time, dish_name)
-VALUES ('Trevor Page', '226-555-4982', '9/10/2023 9:47:00 AM', 'Pepperoni & Cheese Pizza'),
-       ('Trevor Page', '226-555-4982', '9/10/2023 9:47:00 AM', 'Meat Lovers Pizza'),
-       ('John Doe', '555-555-9498', '9/10/2023 13:20:00 AM', 'Vegetarian Pizza'),
-       ('John Doe', '555-555-9498', '9/10/2023 13:20:00 AM', 'Meat Lovers Pizza'),
-       ('John Doe', '555-555-9498', '9/10/2023 13:20:00 AM', 'Meat Lovers Pizza'),
-       ('Trevor Page', '226-555-4982', '9/10/2023 9:47:00 AM', 'Meat Lovers Pizza'),
-       ('Trevor Page', '226-555-4982', '9/10/2023 9:47:00 AM', 'Hawaiian Pizza'),
-       ('John Doe', '555-555-9498', '10/10/2023 10:37:00 AM', 'Vegetarian Pizza'),
-       ('John Doe', '555-555-9498', '10/10/2023 10:37:00 AM', 'Vegetarian Pizza'),
-       ('John Doe', '555-555-9498', '10/10/2023 10:37:00 AM', 'Vegetarian Pizza'),
-       ('John Doe', '555-555-9498', '10/10/2023 10:37:00 AM', 'Hawaiian Pizza');
+INSERT INTO customers (customer_id, customer_name, phone_number)
+VALUES (1, 'Trevor Page', '226-555-4982'),
+       (2, 'John Doe', '555-555-9498');
 
-INSERT INTO pizzas (dish_name, price)
-VALUES ('Pepperoni & Cheese', 7.99),
-       ('Vegetarian', 9.99),
-       ('Meat Lovers', 14.99),
-       ('Hawaiian ', 12.99);
-       
--- *************************************** /\ Table creation and data population code above /\ *******************************************
+INSERT INTO orders (customer_id, dish_id, order_date_time)
+VALUES (1, 11, '2023/9/10 9:47:00'),
+       (1, 13, '2023/9/10 9:47:00'),
+       (2, 12, '2023/9/10 13:20:00'),
+       (2, 13, '2023/9/10 13:20:00'),
+       (2, 13, '2023/9/10 13:20:00'),
+       (1, 13, '2023/9/10 9:47:00'),
+       (1, 14, '2023/9/10 9:47:00'),
+       (2, 12, '2023/10/10 10:37:00'),
+       (2, 12, '2023/10/10 10:37:00'),
+       (2, 12, '2023/10/10 10:37:00'),
+       (2, 14, '2023/10/10 10:37:00');
 
--- ************************************************* \/ Queries Q4 and Q5 below \/ ******************************************************* 
+INSERT INTO pizzas (dish_id, dish_name, dish_price)
+VALUES (11, 'Pepperoni & Cheese', 7.99),
+       (12, 'Vegetarian', 9.99),
+       (13, 'Meat Lovers', 14.99),
+       (14, 'Hawaiian ', 12.99);
 
--- Q4:
-SELECT customer AS Customer, sum(price) AS Total_Amount FROM orders JOIN pizzas GROUP BY customer;
 
--- Q5
-SELECT  customer, order_date_time, price FROM orders JOIN pizzas ORDER BY customer;
+-- *** Q4 ***
+
+SELECT 
+	customers.customer_name, 
+    SUM(pizzas.dish_price) AS total_amount
+FROM 
+	customers 
+JOIN 
+	orders ON customers.customer_id = orders.customer_id
+JOIN 
+	pizzas ON orders.dish_id = pizzas.dish_id
+GROUP BY 
+	customers.customer_name;
+
+-- *** Q5 ***
+
+SELECT 
+	customers.customer_name, 
+    orders.order_date_time, 
+    SUM(pizzas.dish_price) AS total_amount
+FROM 
+	customers 
+JOIN 
+	orders ON customers.customer_id = orders.customer_id
+JOIN 
+	pizzas ON orders.dish_id = pizzas.dish_id
+GROUP BY 
+	customers.customer_name, orders.order_date_time;
+
 
